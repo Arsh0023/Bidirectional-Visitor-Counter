04805362baba83b9dd07be5456d3724d06db9413
Arshpreet <arsharsh2391@gmail.com>
2021-10-17 21:42:43 +0530

    added reset button - APIs left to implement

--- counter_app/base_app/urls.py
+++ counter_app/base_app/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path,reverse
 from . import views
+
+app_name = 'base_app'
 
 urlpatterns = [
     path('',views.index,name='home'),
+    path('reset/', views.reset,name='reset'),
 ]
--- counter_app/base_app/views.py
+++ counter_app/base_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Person
+from django.urls import reverse
+from django.http import HttpResponse,HttpResponseRedirect
+
 # Create your views here.
+
 def index(request):
     persons = Person.objects.all()
     return_dict = {
@@ -8,3 +12,13 @@ def index(request):
     }
     print(persons)
     return render(request,'index.html',context=return_dict)
+
+def reset(request):
+    persons = Person.objects.all()
+    p = persons[0]
+
+    p.visitors_in = 0
+    p.visitors_out = 0
+    p.save()
+
+    return HttpResponseRedirect(reverse('base_app:home'))
